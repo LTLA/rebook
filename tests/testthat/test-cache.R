111a38fbdc78e01e75d0b22bcc65b56a3714245e
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-05-17 17:01:25 -0700

    Fix to work around testthat's weird environment issues.

--- tests/testthat/test-cache.R
+++ tests/testthat/test-cache.R
@@ -10,7 +10,7 @@ test_that("first attempt will compile the report", {
     env <- new.env()
     out <- capture.output(extractCached(tmprmd, chunk="godzilla-1954", object="godzilla", envir=env))
     expect_identical(env$godzilla, "RAWR!")
-    
+
     expect_true(any(grepl("godzilla-1954", out)))
     expect_false(any(grepl("ghidorah-1964", out)))
 })
@@ -23,7 +23,7 @@ test_that("multiple objects can be retrieved", {
 
     expect_true(any(grepl("godzilla-1954", out)))
     expect_true(any(grepl("ghidorah-1964", out)))
-    
+
     # It doesn't pick up the other chunks.
     expect_false(any(grepl("msg <-", out)))
     expect_false(any(grepl("plot\\(", out)))
@@ -33,7 +33,7 @@ test_that("later objects can be successfully retrieved", {
     env <- new.env()
     out <- capture.output(extractCached(tmprmd, chunk="godzilla-2014", object="godzilla", envir=env))
     expect_identical(env$godzilla, "I'm back.")
-    
+
     expect_true(any(grepl("godzilla-1954", out)))
     expect_true(any(grepl("ghidorah-1964", out)))
     expect_true(any(grepl("godzilla-1978", out)))
