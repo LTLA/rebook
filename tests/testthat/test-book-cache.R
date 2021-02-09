a3e68c67aea558be6d75c0a0b1701305597de462
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-02-09 00:22:40 -0800

    Minor fix to the tests.

--- tests/testthat/test-book-cache.R
+++ tests/testthat/test-book-cache.R
@@ -2,9 +2,14 @@
 # library(testthat); library(rebook); source("test-book-cache.R")
 
 test_that("getBookCache works as expected", {
+    old <- Sys.getenv("REBOOK_REBOOK_CACHE", NA)
+    Sys.unsetenv("REBOOK_REBOOK_CACHE")
+
     out <- getBookCache("rebook")
     expect_match(out, "rebook.*rebook")
     expect_identical(basename(out), as.character(packageVersion('rebook')))
+
+    Sys.setenv(REBOOK_REBOOK_CACHE=old)
 })
 
 test_that("getBookCache responds to environment variables", {
@@ -12,7 +17,7 @@ test_that("getBookCache responds to environment variables", {
     Sys.setenv(REBOOK_REBOOK_CACHE="WHEE")
 
     out <- getBookCache("rebook")
-    expect_identical(out, old)
+    expect_identical(out, "WHEE")
 
     Sys.setenv(REBOOK_REBOOK_CACHE=old)
 })
